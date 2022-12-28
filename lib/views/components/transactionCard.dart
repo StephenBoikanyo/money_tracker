20c6d0941543e70b630ee601084d7123e6fd9adb
StephenBoikanyo <StephenBoikanyo69@gmail.com>
2022-12-28 10:22:31 +0200

    Added modular transaction card

---
+++ lib/views/components/transactionCard.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'constants.dart';
+import 'package:akar_icons_flutter/akar_icons_flutter.dart';
+import 'package:intl/intl.dart';
+
+class TransactionCard extends StatelessWidget {
+  final IconData? transactionIcon;
+  final String? title;
+  final int? amount;
+  final DateTime? date;
+  final DateTime? time;
+
+  TransactionCard(
+      {required this.title,
+      required this.amount,
+      required this.date,
+      required this.time,
+      required this.transactionIcon});
+
+  @override
+  Widget build(BuildContext context) {
+    String? value_ = amount.toString();
+
+    String date_  = DateFormat.yMMMd().format(DateTime.now());
+    String? time_ = DateFormat.jm().format(DateTime.now());
+
+    return ListTile(
+      leading: CircleAvatar(
+        backgroundColor: Colors.blueGrey,
+        radius: 40,
+        child: Icon(transactionIcon),
+      ),
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text('$title',
+              style: kTitleTextStyle.copyWith(
+                  fontSize: 18, fontWeight: FontWeight.bold)),
+          Text('R $value_ ',
+              style: kTitleTextStyle.copyWith(
+                  fontSize: 18, fontWeight: FontWeight.bold)),
+        ],
+      ),
+      subtitle: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [Text(date_), Text(time_)],
+      ),
+    );
+  }
+}
