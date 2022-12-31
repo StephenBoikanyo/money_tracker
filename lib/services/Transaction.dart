d503dc9125dd5c125337a65e7c5a9d4669d1db43
Stephen Boikanyo <StephenBoikanyo69@Gmail.com>
2022-12-31 19:59:47 +0200

    Created class for transaction handling

---
+++ lib/services/Transaction.dart
@@ -0,0 +1,30 @@
+import 'services.dart';
+
+class TransactionService {
+
+  late List<BankTransaction> transactions;
+
+  late double totalDeposit = 0;
+  late double totalWithdrawal = 0;
+
+
+  double totalDeposits(transactions) {
+    for (int i = 0; i < transactions.length; i++) {
+      if (transactions[i].amount >= 0) {
+        totalDeposit += transactions[i].amount;
+      }
+    }
+    return totalDeposit;
+  }
+
+
+  double totalWithdrawals(transactions) {
+    for (int i = 0; i < transactions.length; i++) {
+      if (transactions[i].amount <= 0) {
+        totalWithdrawal += transactions[i].amount;
+      }
+    }
+    return totalWithdrawal;
+  }
+
+}
